0a9a190a0362bb475d46983d81367bbed49fe573
Mattias Andersson <mattias.andersson@umu.se>
2019-09-02 13:12:53 +0200

    nyaenv sets terminal title

--- etc/shell_setup.sh
+++ etc/shell_setup.sh
@@ -18,12 +18,16 @@ if [ $MY_BASH_ENV_OS == 'LINUX' ]
     alias open='xdg-open &>/dev/null'
 fi
 
+#Set Bash prompt
 if [ $MY_BASH_ENV_TYPE != 'remote' ]
   then
     export PS1='$(__git_ps1 "\[\e[0;32m\](%s)\[\e[0m\]");'  
   else
     export PS1='\[\033[0;35m\]\h\[\e[0m\]$(__git_ps1 "\[\e[0;32m\](%s)\[\e[0m\]");'
 fi
+
+#Set Bash title
+export PROMPT_COMMAND='printf "\033]0;%s:%s:%s\007" "${USER}" "${NYAENV}" "${PWD/#$HOME/\~}"'
 
 if(command_exists "emacs")
   then
