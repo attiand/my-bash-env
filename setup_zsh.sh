36f858a0b6ae296c35f0be08d3a0794d6940fc62
Mattias Andersson <mattias.j.andersson@gmail.com>
2024-06-20 19:41:08 +0200

    fixes for toolbox

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -114,11 +114,12 @@ export PATH=$BASEDIR/bin:$PATH
 export ACKRC=$BASEDIR/etc/ackrc
 export BAT_CONFIG_PATH=$BASEDIR/etc/batrc
 
+if [ ! -f /run/.toolboxenv ]; then
 PROMPT='$(kube_ps1)'$PROMPT
-export KUBE_PS1_SYMBOL_ENABLE=false
-export KUBE_PS1_PREFIX=[
-export KUBE_PS1_SUFFIX=]
-
+    export KUBE_PS1_SYMBOL_ENABLE=false
+    export KUBE_PS1_PREFIX=[
+    export KUBE_PS1_SUFFIX=]
+fi
 
 bindkey "\e[3B" dirhistory_zle_dirhistory_up
 bindkey "\e[1;3B" dirhistory_zle_dirhistory_up
