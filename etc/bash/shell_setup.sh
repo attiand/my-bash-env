45d0dc419e704790c34bf8fde5ce39271ac2cb11
Mattias Andersson <mattias.andersson@umu.se>
2024-10-03 14:21:56 +0200

    fixed arcrc rules

--- etc/bash/shell_setup.sh
+++ etc/bash/shell_setup.sh
@@ -14,13 +14,13 @@ fi
 
 
 #Set Bash title
-export PROMPT_COMMAND='printf "\033]0;%s:%s:%s\007" "${USER}" "${NYAENV}" "${PWD/#$HOME/\~}"'
+#export PROMPT_COMMAND='printf "\033]0;%s:%s:%s\007" "${USER}" "${NYAENV}" "${PWD/#$HOME/\~}"'
 
 export EDITOR=vim
 
-if [ -x "$(command -v starship)" ]; then
-  eval "$(starship init bash)"
-else
+#if [ -x "$(command -v starship)" ]; then
+#  eval "$(starship init bash)"
+#else
   if [ $MY_BASH_ENV_OS == 'LINUX' ]
     then
     if [ "$(. /etc/os-release; echo $NAME)" = "Ubuntu" ]; then
@@ -37,4 +37,4 @@ else
         export PS1='\[\033[0;35m\]\h\[\e[0m\]$(__git_ps1 "\[\e[0;32m\](%s)\[\e[0m\]");'
     fi
   fi
-fi
+#fi
