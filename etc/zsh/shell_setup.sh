549ee5abafeb3d76b7da7aea76bcce572c188aab
Mattias Andersson <mattias.j.andersson@gmail.com>
2024-12-04 21:38:06 +0100

    added --multi to fzf

--- etc/zsh/shell_setup.sh
+++ etc/zsh/shell_setup.sh
@@ -2,4 +2,4 @@ alias difftool="code --diff"
 export EDITOR=vim
 alias open='xdg-open &>/dev/null'
 
-export FZF_DEFAULT_OPTS="--preview 'bat --color=always {}'"
+export FZF_DEFAULT_OPTS="--multi --preview 'bat --color=always {}'"
